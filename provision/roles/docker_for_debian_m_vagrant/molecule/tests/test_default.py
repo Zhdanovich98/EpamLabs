70afb678cdd67370c8d7a4b5466ad695d385e0a5
jdanovichii <you@example.com>
2021-07-08 10:06:24 +0300

    update tests

--- provision/roles/docker_for_debian_m_vagrant/molecule/tests/test_default.py
+++ provision/roles/docker_for_debian_m_vagrant/molecule/tests/test_default.py
@@ -5,12 +5,6 @@ testinfra_hosts = (testinfra.utils.ansible_runner.AnsibleRunner(
         os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all'))
 
 
-@pytest.mark.parametrize('pkg', ['docker'])
-def test_pkg(host, pkg):
-    package = host.package(pkg)
-    assert package.is_installed
-
-
 @pytest.mark.parametrize('svc', ['docker'])
 def test_svc(host, svc):
     service = host.service(svc)
